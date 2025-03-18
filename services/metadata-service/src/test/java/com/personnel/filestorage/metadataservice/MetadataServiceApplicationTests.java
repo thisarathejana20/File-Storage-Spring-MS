42c69197bb68191044f717cb20c7243df1ef269a
thisaramax20 <thisarathejana20@gmail.com>
2025-03-18 20:40:52 +0530

    first commit

---
+++ services/metadata-service/src/test/java/com/personnel/filestorage/metadataservice/MetadataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personnel.filestorage.metadataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
